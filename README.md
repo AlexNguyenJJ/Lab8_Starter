ea083cbb397e2bfa940a346a6e811b65b142f9ec
AlexNguyenJJ <atn012@ucsd.edu>
2021-11-13 23:52:18 -0800

    Finished Lab

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Lab 8 - Starter
 
+Author: Alex Nguyen
+
 ## 1
 I would fit my automated tests 
 **within a Github action that runs whever code is pushed.** 
@@ -19,3 +21,11 @@ I wouldn't because this case is just testing if one function is returning the
 correct output, not a multitude of different variables being passed through that
  emulates a user using the web page. Just testing if a function is returning the
  correct output would be a case for unit testing if not a custom test case.
+
+## 3
+Yes I would because a unit test can just test if the feature is working as intended
+and so just testing if a message is being able to be sent and received 
+
+## 4
+Yes I would because this is a simple enough test where I can just test one part 
+that isn't being affected by other moving parts. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('matches valid phone numbers return TRUE', () => {
+  expect(functions.isPhoneNumber("(512)815-9890)")).toBe(true);
+});
+
+test('matches valid phone numbers return TRUE', () => {
+  expect(functions.isPhoneNumber("(550)491-8750)")).toBe(true);
+});
+
+test('matches valid phone numbers return FALSE', () => {
+  expect(functions.isPhoneNumber("string")).toBe(false);
+});
+
+test('matches valid phone numbers return FALSE', () => {
+  expect(functions.isPhoneNumber(true)).toBe(false);
+});
+
+test('test valid emails return TRUE', () => {
+  expect(functions.isEmail("testcase1@gmail.com")).toBe(true);
+});
+
+test('test valid emails return TRUE', () => {
+  expect(functions.isEmail("pizza@yahoo.com")).toBe(true);
+});
+
+test('test valid emails return FALSE', () => {
+  expect(functions.isEmail("random word")).toBe(false);
+});
+
+test('test valid emails return FALSE', () => {
+  expect(functions.isEmail("12951")).toBe(false);
+});
+
+test('test strong passwords return TRUE', () => {
+  expect(functions.isStrongPassword("spassw0rd")).toBe(true);
+});
+
+test('test strong passwords return TRUE', () => {
+  expect(functions.isStrongPassword("aboagh13_")).toBe(true);
+});
+
+test('test strong passwords return FALSE', () => {
+  expect(functions.isStrongPassword("_2302hgns")).toBe(false);
+});
+
+test('test strong passwords return FALSE', () => {
+  expect(functions.isStrongPassword("REALLYLONGPASSWORD")).toBe(false);
+});
+
+test('test valid date return TRUE', () => {
+  expect(functions.isDate("12/12/2049")).toBe(true);
+});
+
+test('test valid date return TRUE', () => {
+  expect(functions.isDate("5/9/1482")).toBe(true);
+});
+
+test('test valid date return FALSE', () => {
+  expect(functions.isDate("today")).toBe(false);
+});
+
+test('test valid date return FALSE', () => {
+  expect(functions.isDate("September 9, 999")).toBe(false);
+});
+
+test('test valid Hex Color return TRUE', () => {
+  expect(functions.isHexColor("ff7799")).toBe(true);
+});
+
+test('test valid Hex Color return TRUE', () => {
+  expect(functions.isHexColor("f0f")).toBe(true);
+});
+
+test('test valid Hex Color return FALSE', () => {
+  expect(functions.isHexColor("fffffffff")).toBe(false);
+});
+
+test('test valid Hex Color return FALSE', () => {
+  expect(functions.isHexColor("blue")).toBe(false);
+});
